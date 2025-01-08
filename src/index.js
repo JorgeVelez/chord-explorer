264e1e7f0c2f0d803acee8fc96c29af822887d8f
Jorge Velez <jorgeluisvelez@gmail.com>
2025-01-08 14:59:10 -0600

    first changes

--- src/index.js
+++ src/index.js
@@ -14,36 +14,45 @@ const sound = new Howl({
     }
 });
 
-const startNotes = ['C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B']; 
+const startNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']; 
 
-const startNoteSelector = document.querySelector('#start-note');
+//const startNoteSelector = document.querySelector('#start-note');
 const octaveSelector = document.querySelector('#octave');
 const buttons = document.querySelector('.buttons');
+const roots = document.querySelector('.roots');
 const intervalsInChord = document.querySelector('.intervals-in-chord');
 const notesInChord = document.querySelector('.notes-in-chord');
 
 let selectedStartNote = 'C';
-let selectedOctave = '1';
+let selectedOctave = '2';
 let selectedChord;
 
 const app = {
     init() {
-        this.setupStartNotes();
+       // this.setupStartNotes();
         this.setupOctaves();
         this.setupButtons();
+        this.setupRoots();
         this.setupEventListeners();
     },
-    setupStartNotes() {
-        startNotes.forEach(noteName => {
-            let noteNameOption = this.createElement('option', noteName);
-            startNoteSelector.appendChild(noteNameOption);
-        });
-    },
+    // setupStartNotes() {
+    //     startNotes.forEach(noteName => {
+    //         let noteNameOption = this.createElement('option', noteName);
+    //         startNoteSelector.appendChild(noteNameOption);
+    //     });
+    // },
     setupOctaves() {
         for (let i = 1; i <= 4; i++) {
             let octaveNumber = this.createElement('option', i);
             octaveSelector.appendChild(octaveNumber);
         }
+    },
+    setupRoots() {
+        startNotes.forEach(root => {
+            let rootButton = this.createElement('button', root);
+            roots.appendChild(rootButton);
+        });
+        
     },
     setupButtons() {
         const chordNames = entries().map(entry => {
@@ -56,11 +65,20 @@ const app = {
         
     },
     setupEventListeners() {
-        startNoteSelector.addEventListener('change', () => {
-            selectedStartNote = startNoteSelector.value;
-        });
+        // startNoteSelector.addEventListener('change', () => {
+        //     selectedStartNote = startNoteSelector.value;
+        // });
         octaveSelector.addEventListener('change', () => {
             selectedOctave = octaveSelector.value;
+        });
+        roots.addEventListener('click', (event) => {
+            if (event.target.classList.contains('roots')) {
+                //alert("no existe!");
+                return;
+            }
+            this.resetButtons();
+            event.target.style.backgroundColor = "MediumSpringGreen";
+            selectedStartNote = event.target.innerText;
         });
         buttons.addEventListener('click', (event) => {
             if (event.target.classList.contains('buttons')) {
@@ -68,6 +86,67 @@ const app = {
             }
             selectedChord = event.target.innerText;
             this.displayAndPlayChord(selectedChord);
+        });
+
+        addEventListener("keydown", (event) => { 
+           
+            switch (event.key) {
+                case "a":
+                   this.changeRootButton(startNotes[0]);
+                    
+                  break; 
+                case "w":
+                    this.changeRootButton(startNotes[1]);
+                  break;
+                case "s":
+                    this.changeRootButton(startNotes[2]);
+                  break;
+                case "e":
+                    this.changeRootButton(startNotes[3]);
+                  break;
+                case "d":
+                    this.changeRootButton(startNotes[4]);
+                  break;
+                case "f":
+                    this.changeRootButton(startNotes[5]);
+                  break;
+                  case "t":
+                    this.changeRootButton(startNotes[6]);
+                    break;
+                    case "g":
+                        this.changeRootButton(startNotes[7]);
+                        break;
+                      case "y":
+                        this.changeRootButton(startNotes[8]);
+                        break;
+                      case "h":
+                        this.changeRootButton(startNotes[9]);
+                        break;
+                      case "u":
+                        this.changeRootButton(startNotes[10]);
+                        break;
+                      case "j":
+                        this.changeRootButton(startNotes[11]);
+                        break;
+                     
+                default:
+                  return; 
+              }
+        });
+    },
+    resetButtons() {
+        const rootscollection = document.getElementsByClassName("roots")[0].querySelectorAll("button"); ;
+        rootscollection.forEach(rootsbutton => {
+            rootsbutton.style.backgroundColor = "MediumSlateBlue";
+        });
+    },
+    changeRootButton(val) {
+        this.resetButtons();
+        selectedStartNote = val;
+        const rootscollection = document.getElementsByClassName("roots")[0].querySelectorAll("button"); ;
+        rootscollection.forEach(rootsbutton => {
+            if(rootsbutton.innerText==val)
+            rootsbutton.style.backgroundColor = "MediumSpringGreen";
         });
     },
     displayAndPlayChord(selectedChord) {
